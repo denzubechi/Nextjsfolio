8c93c279f5397220b237f72aee9430e00c4038b6
denzubechi <chukwumasamuel371@gmail.com>
2023-06-23 17:24:56 -0700

    feat:added new project

--- mock/projects.js
+++ mock/projects.js
@@ -1,4 +1,14 @@
 export const projects = [
+
+    {
+       
+        img: "https://res.cloudinary.com/dqny2b4gb/image/upload/v1687537322/landijg_page_najlck.png",
+        name: "Konect",
+        text: "A platform that connects Students within their industries for seamless collaboration, growth, and networking opportunities..#Crrently building",
+        tools: "MERN,REDUX,TAILWIND,BOOTSTRAP",
+        link: "https://konect.vercel.app/",
+        github: "#",
+    },
     {
        
         img: "https://res.cloudinary.com/dqny2b4gb/image/upload/v1686423660/portfolio_tqnaze.png",
